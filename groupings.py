644b23c7c20c79a490f690b9197478281c01b932
twileman <twileman@redhat.com>
2021-04-26 22:15:37 -0400

    save to csv try 2

---
+++ groupings.py
@@ -0,0 +1,45 @@
+deep_learning = ["('neural', 'network')"]
+
+machine_learning = [
+    "classif",
+    "cluster",
+    "ensembl",
+    "('gradient', 'boost')",
+    "graph",
+    "knn",
+    "machin",
+    "model",
+    "('mont', 'carlo')",
+    "('natur', 'languag')",
+    "('random', 'forest')",
+    "sentiment",
+    "supervis",
+    "('text', 'mine')",
+    "tree",
+    "unsupervis",
+]
+
+analytics = [
+    "analysi",
+    "anova",
+    "arima",
+    "('analyz', 'data')",
+    "covari",
+    "bayesian",
+    "dataanalyt",
+    "eigenvalu",
+    "eigenvector" "heteroscedast",
+    "('linear', 'regress')",
+    "logist",
+    "multicollinear",
+    "('odd', 'ratio')",
+    "outlier",
+    "precis",
+    "('princip', 'compon')",
+    "recal",
+    "sensit",
+    "specif",
+    "tableau",
+]
+
+management = ["busi", "('busi', 'analyt')", "leadership", "manag", "strateg"]
---
+++ programs.py
@@ -0,0 +1,6 @@
+programs = {
+    "Duke University: MQM": "https://www.fuqua.duke.edu/programs/mqm-business-analytics/curriculum",
+    "NC State University: Institute for Advanced Analytics": "https://analytics.ncsu.edu/?page_id=123",
+    "Wake Forest University: MSBA": "https://business.wfu.edu/masters-in-business-analytics/curriculum/json",
+    "William and Mary: MSBA": "https://graduate.mason.wm.edu/msba/academics/json",
+}
